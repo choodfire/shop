e021795cd281d8c9a620daa85eb6ce5f1e11a21f
Ruslan <zzzlyka221@gmail.com>
2023-02-03 22:24:12 +0500

    catalog completed

--- core/views.py
+++ core/views.py
@@ -1,6 +1,6 @@
 from django.shortcuts import render
 from django.views.generic import ListView, TemplateView
-from .models import Product
+from .models import Product, CATEGORIES
 
 # Create your views here.
 
@@ -8,6 +8,12 @@ class IndexView(ListView):
     model = Product
     template_name = 'core/index.html'
 
-class CatalogView(ListView):
-    model = Product
-    template_name = 'core/index.html'
+class CatalogView(TemplateView):
+    # model = ['CATEGORIES', 'CATEGORIES2', 'CATEGORIES3']
+    template_name = 'core/catalog.html'
+
+    def get_context_data(self, *, object_list=None, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context['categories'] = CATEGORIES
+        return context
+
