67904a96502e622658baa0c9d94a6dd5e40f14d8
Ruslan <zzzlyka221@gmail.com>
2023-02-06 18:24:31 +0500

    store app deleted

--- store/admin.py
+++
@@ -1,8 +0,0 @@
-from django.contrib import admin
-from .models import Store
-# Register your models here.
-
-class StoreAdmin(admin.ModelAdmin):
-    list_display = ('address',)
-
-admin.site.register(Store, StoreAdmin)
